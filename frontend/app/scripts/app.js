0b4b8b95acaf2c5365d751a07d64c4fbf43208e5
Felipe <felipe.piacsek@gmail.com>
2016-05-24 19:31:03 -0300

    Ajustes de tela e websocket.

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -12,40 +12,11 @@ var chatHost = "ws://localhost:5000/chat";
 angular
   .module('chatApp', [
     'ngResource',
-    'ngRoute',
     'ngWebSocket',
     'ngSanitize'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
   .factory('ChatSocket', function($websocket) {
       // Open a WebSocket connection
       var dataStream = $websocket(chatHost);
-
-      var collection = [];
-
-      dataStream.onMessage(function(message) {
-        collection.push(JSON.parse(message.data));
-      });
-
-      var methods = {
-        collection: collection,
-        get: function() {
-          dataStream.send(JSON.stringify({ action: 'get' }));
-        },
-        sendMessage: function(message){
-          dataStream.send(message);
-        }
-      };
-
-      return methods;
-  })
+      return dataStream;
+});
